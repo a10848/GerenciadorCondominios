e59a6319c44e07961135cb61ee08f9c97e3f55c9
Vitor Gonçalves <a10848@alunos.ipca.pt>
2021-05-04 14:52:06 +0100

    impelmentação de todas as classes

---
+++ GerenciadorCondominios.BLL/Models/Apartamento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GerenciadorCondominios.BLL.Models
+{
+    public class Apartamento
+    {
+        public int ApartamentoId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(0, 1000, ErrorMessage = "Valor Inválido")]
+        [Display(Name ="Número")]
+        public int Numero { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(0, 10, ErrorMessage = "Valor Inválido")]
+        public int Andar { get; set; }
+        public string Foto { get; set; }
+        public int MoradorId { get; set; }
+        public virtual User Morador { get; set; }
+        public int ProprietarioId { get; set; }
+        public virtual User Proprietario { get; set; }
+    }
+}
